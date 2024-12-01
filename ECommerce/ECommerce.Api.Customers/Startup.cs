400d6fb13a21596f554b63a97146a20bed2c8893
ritayoussef <98358929+ritayoussef@users.noreply.github.com>
2024-11-30 20:07:07 -0500

    added the necessary services in the startup for the dbcontext, scope the interface and auto mapper

--- ECommerce/ECommerce.Api.Customers/Startup.cs
+++ ECommerce/ECommerce.Api.Customers/Startup.cs
@@ -1,7 +1,12 @@
+using AutoMapper;
+using ECommerce.Api.Customers.Db;
+using ECommerce.Api.Customers.Interfaces;
+using ECommerce.Api.Customers.Providers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,7 +29,13 @@ namespace ECommerce.Api.Customers
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
-        {
+        {     
+            services.AddDbContext<CustomersDbContext>(options =>
+            {
+                options.UseInMemoryDatabase("Customers");
+            });
+            services.AddScoped<ICustomersProvider, CustomersProvider>();
+            services.AddAutoMapper(typeof(Startup));
             services.AddControllers();
         }
 
